ad4caf0c4ae375dbd712f6b24ab8ca69f48af8f5
PAIN「ᴀᴋᴀᴛsᴜᴋɪ」 <81083883+CrucifiedPain@users.noreply.github.com>
2026-06-24 15:40:07 +0530

    Bump version from 0.2.0 to 0.2.1

--- warera/__init__.py
+++ warera/__init__.py
@@ -238,7 +238,7 @@ def __getattr__(name: str) -> typing.Any:
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
 
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 __all__ = [
     # Client
